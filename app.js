cb20caf6c4f058f71501662569e7b6e6b35b0b7c
Muhammad Moiz <muhammad.moiz.26@dartmouth.edu>
2024-01-09 13:31:46 -0500

    fix: handle * and / before + -

--- app.js
+++ app.js
@@ -4,17 +4,29 @@ var current = '';
 var OPS = ['+', '-', '*', '/'];
 
 function compute(expr) {
-  // very small parser, left-to-right, no precedence yet
   var tokens = expr.match(/(\d+\.?\d*|[+\-*/])/g);
   if (!tokens) return 0;
-  var val = parseFloat(tokens[0]);
+  // first pass: * and /
+  var pass1 = [tokens[0]];
   for (var i = 1; i < tokens.length; i += 2) {
     var op = tokens[i];
-    var n = parseFloat(tokens[i+1]);
-    if (op === '+') val += n;
-    else if (op === '-') val -= n;
-    else if (op === '*') val *= n;
-    else if (op === '/') val /= n;
+    var n = tokens[i+1];
+    if (op === '*' || op === '/') {
+      var prev = parseFloat(pass1.pop());
+      var cur = parseFloat(n);
+      pass1.push(String(op === '*' ? prev * cur : prev / cur));
+    } else {
+      pass1.push(op);
+      pass1.push(n);
+    }
+  }
+  // second pass: + and -
+  var val = parseFloat(pass1[0]);
+  for (var j = 1; j < pass1.length; j += 2) {
+    var op2 = pass1[j];
+    var n2 = parseFloat(pass1[j+1]);
+    if (op2 === '+') val += n2;
+    else if (op2 === '-') val -= n2;
   }
   return val;
 }
